b2add5e9434ad127aa199d3f74f44d862033ae56
Ezra Onyinkwa <139281995+ezraonyinkwa@users.noreply.github.com>
2025-11-09 16:08:17 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,12 @@
+USE master;
 
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse; ---Switching into the new database
+
+---Creating the Schemas
+CREATE SCHEMA bronze;
+GO --Separator
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
